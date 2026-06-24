cd74d43f4265d22c7134fd28d7755bf861ec64d6
samakunchan <cedric.badjah@gmail.com>
2026-06-24 16:07:42 +0200

    feat(messages): extension
    - Added date format extension.

--- lib/core/extensions/string_extension.dart
+++ lib/core/extensions/string_extension.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 extension StringExtension on String {
   ///  Transform a text into a slug.
   String slugify() => toLowerCase()
@@ -7,4 +9,18 @@ extension StringExtension on String {
       .replaceAll(RegExp('-+'), '-')
       .replaceAll(RegExp('^-+'), '')
       .replaceAll(RegExp(r'-+$'), '');
+
+  String formatFullDate() {
+    final DateTime? dateTime = DateTime.tryParse(this);
+    if (dateTime == null) return '';
+    return DateFormat('d MMMM yyyy').format(dateTime);
+  }
+
+  String formatTime() {
+    final DateTime? dateTime = DateTime.tryParse(this);
+    if (dateTime == null) return '';
+    return DateFormat('HH:mm').format(dateTime);
+  }
+
+  String getInitialAvatar() => split(' ').where((s) => s.isNotEmpty).map((s) => s[0]).take(2).join().toUpperCase();
 }
